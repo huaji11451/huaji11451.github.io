e74c05c0f0e194ed01843a0d94c2e8e3acff9556
huaji11451 <3251200249@qq.com>
2025-11-07 12:19:04 +0800

    Site updated: 2025-11-07 12:19:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/27/hello-world/","2025/10/27/博客名/","2025/11/07/aquadream_1/"];function toRandomPost(){
+var posts=["2025/10/27/hello-world/","2025/10/27/博客名/","2025/11/07/aquadream/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
